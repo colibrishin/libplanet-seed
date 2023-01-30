de9370e077f6a87fc2f95e8d033d8f494869d7f7
Colibri Shin <colibrishin@gmail.com>
2023-01-30 18:30:40 +0900

    feat: reply `PongMsg` message for `PingMsg` and unknown requests

--- Libplanet.Seed.Executable/Net/Seed.cs
+++ Libplanet.Seed.Executable/Net/Seed.cs
@@ -97,17 +97,17 @@ namespace Libplanet.Seed.Executable.Net
         {
             switch (message)
             {
-                case PingMsg ping:
-                    var pong = new PongMsg { Identity = ping.Identity };
-                    await _transport.ReplyMessageAsync(pong, _runtimeCancellationTokenSource.Token);
-
-                    break;
-
                 case FindNeighborsMsg findNeighbors:
                     var neighbors = new NeighborsMsg(Peers) { Identity = findNeighbors.Identity };
                     await _transport.ReplyMessageAsync(
                         neighbors,
                         _runtimeCancellationTokenSource.Token);
+                    break;
+                default:
+                    // FIXME: Seed responses PongMsg for PingMsg and other messages. At some point,
+                    // Swarm should not sending a message to seed peer.
+                    var pong = new PongMsg { Identity = message.Identity };
+                    await _transport.ReplyMessageAsync(pong, _runtimeCancellationTokenSource.Token);
                     break;
             }
 
